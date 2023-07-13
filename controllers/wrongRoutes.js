bee60983c6792091df1c826553ee36c5cf26ad57
homo-errantium <homo.errantium@gmail.com>
2023-07-14 02:57:52 +0300

    rest 3 errors of test

--- controllers/wrongRoutes.js
+++ controllers/wrongRoutes.js
@@ -1,6 +1,7 @@
 const { NOT_FOUND_CODE } = require('../utils/constants');
 
-module.exports.getWrongRouter = (req, res, next) => {
-  res.status(NOT_FOUND_CODE).send('Запрашиваемый ресурс не найден');
-  next();
+module.exports.getWrongRouter = (req, res) => {
+  res
+    .status(NOT_FOUND_CODE)
+    .send({ message: 'Запрашиваемый ресурс не найден' });
 };
