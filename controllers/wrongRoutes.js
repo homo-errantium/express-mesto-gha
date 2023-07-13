9e04d84b16c3e0b094dc07e83559c1d034e66bc8
homo-errantium <homo.errantium@gmail.com>
2023-07-14 02:49:34 +0300

    check tests

--- controllers/wrongRoutes.js
+++ controllers/wrongRoutes.js
@@ -1,8 +1,6 @@
 const { NOT_FOUND_CODE } = require('../utils/constants');
 
 module.exports.getWrongRouter = (req, res, next) => {
-  res
-    .status(NOT_FOUND_CODE)
-    .json({ message: 'Запрашиваемый ресурс не найден' });
+  res.status(NOT_FOUND_CODE).send('Запрашиваемый ресурс не найден');
   next();
 };
