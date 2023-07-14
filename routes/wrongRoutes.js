5c95435ae6187fa63553bee10e9df6342788a7ef
homo-errantium <homo.errantium@gmail.com>
2023-07-14 03:01:59 +0300

    rest 3 errors of test

--- routes/wrongRoutes.js
+++ routes/wrongRoutes.js
@@ -2,5 +2,6 @@ const wrongRouter = require('express').Router();
 const { getWrongRouter } = require('../controllers/wrongRoutes');
 
 wrongRouter.get('*', getWrongRouter);
+wrongRouter.patch('*', getWrongRouter);
 
 module.exports = { wrongRouter };
