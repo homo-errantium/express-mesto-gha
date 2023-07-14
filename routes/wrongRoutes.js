fb7d582aeb453922ff543972a8170ab97ab5a7aa
homo-errantium <homo.errantium@gmail.com>
2023-07-15 01:55:01 +0300

    finish testing

--- routes/wrongRoutes.js
+++ routes/wrongRoutes.js
@@ -2,6 +2,9 @@ const wrongRouter = require('express').Router();
 const { getWrongRouter } = require('../controllers/wrongRoutes');
 
 wrongRouter.get('*', getWrongRouter);
+wrongRouter.post('*', getWrongRouter);
+wrongRouter.put('*', getWrongRouter);
 wrongRouter.patch('*', getWrongRouter);
+wrongRouter.delete('*', getWrongRouter);
 
 module.exports = { wrongRouter };
