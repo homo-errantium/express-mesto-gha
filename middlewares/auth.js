6183c248383f5c186c606775ef3ad364af9db59d
homo-errantium <homo.errantium@gmail.com>
2023-07-24 18:41:24 +0300

    2 errors by autotest

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { AuthError } = require('../errors/AuthError');
+const AuthError = require('../errors/AuthError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
@@ -20,22 +20,3 @@ module.exports = (req, res, next) => {
   req.user = payload;
   next();
 };
-
-// eslint-disable-next-line consistent-return
-
-// module.exports = (req, res, next) => {
-//   if (!res.cookies) {
-//     return new AuthError('Необходима авторизация');
-//   }
-
-//   const token = res.cookies.jwt;
-//   let payload;
-
-//   try {
-//     payload = jwt.verify(token, 'some-secret-key');
-//   } catch (err) {
-//     return new AuthError('Необходима авторизация');
-//   }
-//   req.user = payload;
-//   next();
-// };
